493b6316c6199f754f53affec469e98160f9ba6e
Neil <neillarys@gmail.com>
2018-10-31 15:43:17 +0530

    Initial commit

---
+++ app/src/main/java/com/reminder/AlarmCursorAdapter.java
@@ -0,0 +1,78 @@
+package com.reminder;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CursorAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.amulyakhare.textdrawable.TextDrawable;
+import com.amulyakhare.textdrawable.util.ColorGenerator;
+import com.reminder.data.AlarmReminderContract;
+
+public class AlarmCursorAdapter extends CursorAdapter {
+
+    private TextView mTitleText, mDateAndTimeText, mRepeatInfoText;
+    private ImageView mActiveImage , mThumbnailImage;
+    private ColorGenerator mColorGenerator = ColorGenerator.DEFAULT;
+    private TextDrawable mDrawableBuilder;
+
+    public AlarmCursorAdapter(Context context, Cursor c) {
+        super(context, c, 0 /* flags */);
+    }
+    @Override
+    public View newView(Context context, Cursor cursor, ViewGroup parent) {
+        return LayoutInflater.from(context).inflate(R.layout.alarm_items, parent, false);
+    }
+
+    @Override
+    public void bindView(View view, Context context, Cursor cursor) {
+
+        mTitleText = (TextView) view.findViewById(R.id.recycle_title);
+        mDateAndTimeText = (TextView) view.findViewById(R.id.recycle_date_time);
+        mActiveImage = (ImageView) view.findViewById(R.id.active_image);
+
+        int titleColumnIndex = cursor.getColumnIndex(AlarmReminderContract.AlarmReminderEntry.KEY_TITLE);
+        int dateColumnIndex = cursor.getColumnIndex(AlarmReminderContract.AlarmReminderEntry.KEY_DATE);
+        int timeColumnIndex = cursor.getColumnIndex(AlarmReminderContract.AlarmReminderEntry.KEY_TIME);
+        int activeColumnIndex = cursor.getColumnIndex(AlarmReminderContract.AlarmReminderEntry.KEY_ACTIVE);
+
+        String title = cursor.getString(titleColumnIndex);
+        String date = cursor.getString(dateColumnIndex);
+        String time = cursor.getString(timeColumnIndex);
+        String active = cursor.getString(activeColumnIndex);
+
+        mTitleText.setText(title);
+
+        if (date != null){
+            String dateTime = date + " " + time;
+            setReminderDateTime(dateTime);
+        }else{
+            mDateAndTimeText.setText("Date not set");
+        }
+
+        if (active != null){
+            setActiveImage(active);
+        }else{
+            mActiveImage.setImageResource(R.drawable.ic_notifications_off_grey600_24dp);
+        }
+    }
+
+    // Set date and time views
+    public void setReminderDateTime(String datetime) {
+        mDateAndTimeText.setText(datetime);
+    }
+
+    // Set active image as on or off
+    public void setActiveImage(String active){
+        if(active.equals("true")){
+            mActiveImage.setImageResource(R.drawable.ic_notifications_on_white_24dp);
+        }else if (active.equals("false")) {
+            mActiveImage.setImageResource(R.drawable.ic_notifications_off_grey600_24dp);
+        }
+
+    }
+}
